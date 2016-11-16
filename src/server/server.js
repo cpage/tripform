8a3c0b78dcedddf783dfcdc065a9b7768824cb8e
Page <321522724@MAPLE.fg.rbc.com>
2016-11-16 16:13:05 -0500

    added a bit more error handling to server.js

--- src/server/server.js
+++ src/server/server.js
@@ -5,10 +5,14 @@ var config = require('./app/config');
 var port = process.env.PORT || config.env.defaultPort;
 
 db.connect(config.mongo.url).then(function (results) {
-    var app = require('./app');
-    app.listen(port, function () {
-        logger.info('server listening on port ' + port);
-    });
+    try {
+        var app = require('./app');
+        app.listen(port, function () {
+            logger.info('server listening on port ' + port);
+        });
+    } catch (ex) {
+        logger.error(ex);
+    }
 }).catch(function (err) {
-    logger.error('error connecting to db' + err + '. Stack tracke: ' + err.stackTrace);
+    logger.error('error connecting to db' + err + '. Stack trace: ' + err.stackTrace);
 });
