d6572dbf622c397579a379920b609d4bcb45b6fd
Page <321522724@MAPLE.fg.rbc.com>
2016-12-13 10:20:45 -0500

    fixed caching issue in departure ctrl

--- src/server/app/controllers/departureController.js
+++ src/server/app/controllers/departureController.js
@@ -5,37 +5,39 @@ var _ = require('lodash');
 var envConfig = require('../config/environment');
 var memCache = require('../lib/cache');
 
-module.exports = function () {
+module.exports = function() {
 
 
-    var getAll = function (req, res) {
-        memCache.wrap("allDepartures", function () {
+    var getAll = function(req, res) {
+        memCache.wrap("allDepartures", function() {
             return beacon.getDepartures();
-        }).then(function (departures) {
+        }).then(function(departures) {
             res.json(departures);
         });
     };
 
-    var getMyDepartures = function (req, res) {
-        beacon.getDepartures().then(function (departures) {
-            userRepo.getUsersByTeam(req.user.p15Team).then(function (teamUsers) {
-                let teamIds = teamUsers.map(function (t) {
-                    return t.p15Id;
+    var getMyDepartures = function(req, res) {
+
+        memCache.wrap("allDepartures", function() { return beacon.getDepartures(); })
+            .then(function(departures) {
+                userRepo.getUsersByTeam(req.user.p15Team).then(function(teamUsers) {
+                    let teamIds = teamUsers.map(function(t) {
+                        return t.p15Id;
+                    });
+
+                    var myDepartures = _.filter(departures, function(departure) {
+                        let allIds = _.concat(departure.SubmitterContactIDs.GuideContactID, departure.ApproversContactIDs.ApproverContactID);
+                        let matchingIds = _.intersection(allIds, teamIds);
+
+                        return matchingIds.length > 0;
+                    });
+
+                    res.json(myDepartures);
                 });
 
-                _.remove(departures, function (departure) {
-                    let allIds = _.concat(departure.SubmitterContactIDs.GuideContactID, departure.ApproversContactIDs.ApproverContactID);
-                    let matchingIds = _.intersection(allIds, teamIds);
 
-                    return matchingIds.length === 0;
-                });
-
-                res.json(departures);
-            });
-
-
-        })
-            .catch(function (err) {
+            })
+            .catch(function(err) {
                 console.log('departureController:Error:>>' + err + '<<');
                 res.status(500).send(err.message);
             });
