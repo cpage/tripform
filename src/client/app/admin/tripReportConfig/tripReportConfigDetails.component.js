49470027ca01ce160fe173a668f7e432402acece
Chris Page <Chris Page>
2016-12-14 21:21:01 -0500

    some simple functionality for config

--- src/client/app/admin/tripReportConfig/tripReportConfigDetails.component.js
+++ src/client/app/admin/tripReportConfig/tripReportConfigDetails.component.js
@@ -21,8 +21,32 @@
     function tripReportConfigDetailsController() {
         var $ctrl = this;
 
+        $ctrl.addHeading = function() {
+            $ctrl.tripReportConfig.configHeadings.push({
+                id: _.maxBy($ctrl.tripReportConfig.configHeadings, 'id').id + 1,
+                text: '',
+                description: '',
+                configQuestions: [{
+                    id: 1,
+                    question: '',
+                    responseType: '',
+                    isRequired: false,
+                    p15FieldName: ''
+                }]
+            });
+        };
 
+        $ctrl.addQuestion = function(heading) {
+            heading.configQuestions.push({
+                id: _.maxBy(heading.configQuestions, 'id').id + 1,
+                question: '',
+                responseType: '',
+                isRequired: false,
+                p15FieldName: ''
+            });
+        }
         ////////////////
+
 
         $ctrl.$onInit = function () {
             console.log('in tripReportConfigDetails controller...');
@@ -32,19 +56,19 @@
                     text: "heading 1",
                     description: "description 1",
                     configQuestions: [{
-                        id: 'q1',
+                        id: 1,
                         question: 'what is question 1?',
                         responseType: '3',
                         isRequired: true,
                         p15FieldName: ''
                     }, {
-                        id: 'q2',
+                        id: 2,
                         question: 'what is question 2?',
                         responseType: '1',
                         isRequired: false,
                         p15FieldName: ''
                     }, {
-                        id: 'q3',
+                        id: 3,
                         question: 'what is question 1?',
                         responseType: '2',
                         isRequired: true,
@@ -55,49 +79,47 @@
                     text: "heading 2",
                     description: "description 2",
                     configQuestions: [{
-                        id: 'q1',
+                        id: 1,
                         question: 'what is question 1?',
-                        responseType: '2',
+                        responseType: '3',
                         isRequired: true,
                         p15FieldName: ''
                     }, {
-                        id: 'q2',
+                        id: 2,
                         question: 'what is question 2?',
                         responseType: '1',
                         isRequired: false,
                         p15FieldName: ''
                     }, {
-                        id: 'q3',
+                        id: 3,
                         question: 'what is question 1?',
-                        responseType: '3',
+                        responseType: '2',
                         isRequired: true,
                         p15FieldName: ''
                     }]
-
                 }, {
                     id: 3,
                     text: "heading 3",
                     description: "description 3",
                     configQuestions: [{
-                        id: 'q1',
+                        id: 1,
                         question: 'what is question 1?',
-                        responseType: '2',
+                        responseType: '3',
                         isRequired: true,
                         p15FieldName: ''
                     }, {
-                        id: 'q2',
+                        id: 2,
                         question: 'what is question 2?',
                         responseType: '1',
                         isRequired: false,
                         p15FieldName: ''
                     }, {
-                        id: 'q3',
+                        id: 3,
                         question: 'what is question 1?',
-                        responseType: '3',
+                        responseType: '2',
                         isRequired: true,
                         p15FieldName: ''
                     }]
-
                 }, ]
             };
         };
